0a57d78ca2a571b8759f8702c936bfad94170ed1
HaendelR <89445756+HaendelR@users.noreply.github.com>
2023-07-28 18:16:55 +0300

    clean users model

--- models/users.js
+++ models/users.js
@@ -1,18 +1,18 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+import { hash, compare } from "bcryptjs";
+import { sign, verify } from "jsonwebtoken";
 
 var collections = "users";
 
-exports.getusers = function (req, res) {
+export function getusers(req, res) {
   var db = req.db;
   var collection = db.get(collections);
 
   collection.find({}, {}, function (e, docs) {
     res.status(200).json(docs);
   });
-};
+}
 
-exports.inscription = async function (req, res) {
+export async function inscription(req, res) {
   try {
     var db = req.db;
     var collection = db.get(collections);
@@ -20,37 +20,48 @@ exports.inscription = async function (req, res) {
     var user = {
       name: req.body.name,
       surname: req.body.surname,
-      numberPhone: req.body.numberPhone,
-      salary: req.body.salary,
       genre: req.body.genre,
+      birthDate: req.body.birthDate,
+      adress: req.body.adress,
       email: req.body.email,
-      password: await bcrypt.hash(req.body.password, 10),
-      role: req.body.role,
-      garageName: req.body.garageName,
-      garageLocation: req.body.garageLocation
+      contact: req.body.contact,
+      username: req.body.username,
+      password: await hash(req.body.password, 10),
+      createdAt: new Date(),
     };
 
     let duplemail = await collection.findOne({
-      email: req.body.email
+      email: req.body.email,
     });
 
-    if(duplemail) {
-      res.status(400).json({error: "email déjà utilisé"})
+    if (duplemail) {
+      res.status(400).json({ error: "email déjà utilisé" });
     } else {
       collection.insert(user, function (e, docs) {
         res.json(docs);
       });
     }
 
+    let duplusername = await collection.findOne({
+      username: req.body.username,
+    });
+
+    if (duplusername) {
+      res.status(400).json({ error: "Nom d'utilisateur déjà utilisé" });
+    } else {
+      collection.insert(user, function (e, docs) {
+        res.json(docs);
+      });
+    }
   } catch (error) {
     res.status(400).json({ error });
   }
-};
+}
 
-exports.login = async function (req, res) {
+export async function login(req, res) {
   try {
     var user = {
-      email: req.body.email,
+      username: req.body.username,
     };
 
     var db = req.db;
@@ -58,20 +69,20 @@ exports.login = async function (req, res) {
 
     collection.findOne(user, async function (e, docs) {
       if (docs) {
-        const pass = await bcrypt.compare(req.body.password, docs.password);
+        const pass = await compare(req.body.password, docs.password);
         if (pass) {
           var usertoken = {
             email: docs.email,
             name: docs.name,
             surname: docs.surname,
-            role: docs.role,
-            numberPhone: docs.numberPhone,
             genre: docs.genre,
-            garageName: docs.garageName,
-            garageLocation: docs.garageLocation
-          }
+            contact: docs.contact,
+            birthDate: docs.birthDate,
+            adress: docs.adress,
+            username: docs.username,
+          };
 
-          jwt.sign(usertoken, "randomString", (err, token) => {
+          sign(usertoken, "randomString", (err, token) => {
             if (err) throw err;
             res.status(200).json({ token: token, error: "" });
           });
@@ -89,38 +100,36 @@ exports.login = async function (req, res) {
   }
 }
 
-exports.userconnecte = async function (req, res) {
+export async function userconnecte(req, res) {
   const head = req.headers.authorization;
 
-  const token = head?.split(' ')[1];
+  const token = head?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Token not found" });
 
   try {
-    const decodedtoken = jwt.verify(token, "randomString");
+    const decodedtoken = verify(token, "randomString");
 
     res.json(decodedtoken);
-  } catch(e) {
-    res.status(500).send({ message: "Invalid Token"});
-  }
-  
-}
-
-exports.getUserWhereGarage = async function (req, res) {
-  try {
-    var db = req.db;
-    var collection = db.get(collections);
-
-    collection.find({
-      garageName: req.params.garageName,
-      garageLocation: req.params.garageLocation,
-      role: {$ne : "client"}
-    },
-    {},
-    function(e, docs) {
-      res.status(200).json(docs);
-    });
-
-  } catch(e) {
-
+  } catch (e) {
+    res.status(500).send({ message: "Invalid Token" });
   }
 }
+
+// exports.getUserWhereGarage = async function (req, res) {
+//   try {
+//     var db = req.db;
+//     var collection = db.get(collections);
+
+//     collection.find(
+//       {
+//         garageName: req.params.garageName,
+//         garageLocation: req.params.garageLocation,
+//         role: { $ne: "client" },
+//       },
+//       {},
+//       function (e, docs) {
+//         res.status(200).json(docs);
+//       }
+//     );
+//   } catch (e) {}
+// };
--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,26 @@
-var express = require('express');
-var router = express.Router();
-var users = require('../models/users');
+import { Router } from "express";
+import { getusers, inscription, login, userconnecte } from "../models/users";
 
-router.get('/allusers', async function(req, res) {
-  users.getusers(req, res);
+var router = Router();
+
+router.get("/allusers", async function (req, res) {
+  getusers(req, res);
 });
 
-router.post('/adduser', async function(req, res) {
-  users.inscription(req,res);
+router.post("/adduser", async function (req, res) {
+  inscription(req, res);
 });
 
-router.post('/login', async function(req, res) {
-  users.login(req, res);
-})
+router.post("/login", async function (req, res) {
+  login(req, res);
+});
 
-router.get('/me', async function(req, res) {
-  users.userconnecte(req, res);
-})
+router.get("/me", async function (req, res) {
+  userconnecte(req, res);
+});
 
-router.get('/userByGarage/:garageName/:garageLocation', async function(req, res) {
-  users.getUserWhereGarage(req, res);
-})
+// router.get('/userByGarage/:garageName/:garageLocation', async function(req, res) {
+//   users.getUserWhereGarage(req, res);
+// })
 
-
-module.exports = router;
+export default router;
